de88ca9aa34f227b9bf2e249c5da05cd2043586d
Shreyas Kharbanda <54546748+Alphacode18@users.noreply.github.com>
2020-05-30 19:23:06 +0530

    Scrape function added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer');
 
-
 async function scrape(url) {
-    
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const titles = page.evaluate(() => {
+        return document.getElementsByClassName('titleColumn').innerText;
+    });
+    for (let i = 0; i < (await titles).length; i++) {
+        await console.log(titles[i]);
+    }
 }
+
+scrape('https://www.imdb.com/chart/moviemeter/?sort=ir,desc&mode=simple&page=1');
