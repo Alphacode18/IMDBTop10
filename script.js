2477ea4cc3844de7218a777f77b2a24c0181c8d0
Shreyas Kharbanda <54546748+Alphacode18@users.noreply.github.com>
2020-05-31 20:20:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ async function scrape(url) {
     const page = await browser.newPage();
     await page.goto(url, {waitUntil: 'networkidle0'});
     const data = await page.evaluate(() => {
-        const title = document.querySelector('td[class=titleColumn]').innerText;
-        return title.substring(0, title.indexOf('('));
+        const titleArray = [];
+        const titles = document.getElementsByClassName('titleColumn');
+        for (let i = 0; i < 10; i++) {
+            titleArray.push(titles[i].innerText);
+        }
+        return titleArray;
     });
-    console.log(data);
+    data.array.forEach(element => {
+        console.log(element);
+    });
     await browser.close();
 }
 
-const movies = scrape('https://www.imdb.com/chart/moviemeter/?sort=ir,desc&mode=simple&page=1');
-
-console.log(movies);
+scrape('https://www.imdb.com/chart/moviemeter/?sort=ir,desc&mode=simple&page=1');
